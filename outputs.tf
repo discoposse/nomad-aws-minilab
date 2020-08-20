6858e68505d46bedbf4279c54cd745b7ba20191b
Eric Wright <eric.wright@turbonomic.com>
2020-08-19 23:32:13 -0400

    adding outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "instance_ip_addr" {
+output "private_ip_addr" {
     value = aws_instance.nomad-node[*].private_ip
+}
+
+output "public_ip_addr" {
+    value = aws_instance.nomad-node[*].public_ip
 }
