fc944791d31acb61b121fc2af4a8c69634017e90
Eric Wright <eric.wright@turbonomic.com>
2020-08-19 20:56:16 -0400

    change VPC

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
     ami = var.nomad_node_ami_id
     instance_type = var.nomad_node_instance_size
     key_name = var.aws_key_name
+    vpc_id = var.aws_vpc_id
 
     tags = {
         Terraform = "true"
@@ -9,3 +10,4 @@ resource "aws_instance" "web" {
         Turbonomic = "true"
     }
 }
+
