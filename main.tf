98c2f4ab99243a649d0cc012ef51db1e7443dceb
Eric Wright <eric.wright@turbonomic.com>
2020-08-19 20:49:54 -0400

    removing old school interpolation format

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-    ami = "var.nomad_node_ami_id"
-    instance_type = "var.nomad_node_instance_size"
-    key_name = "var.aws_key_name"
+    ami = var.nomad_node_ami_id
+    instance_type = var.nomad_node_instance_size
+    key_name = var.aws_key_name
 
     tags = {
         Terraform = "true"
