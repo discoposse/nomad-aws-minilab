d963bbc0c5c50dd1e3471264a2d2dde65dda4aaa
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 23:09:46 -0400

    setting vpc

--- main.tf
+++ main.tf
@@ -4,10 +4,7 @@ resource "aws_instance" "nomad-node" {
     ami = var.nomad_node_ami_id
     instance_type = var.nomad_node_instance_size
     key_name = var.aws_key_name
-
-    #for_each = aws_subnet.nomad-lab-pub
-    #subnet_id = each.value.id
-    #subnet_id = aws_subnet.nomad-lab-pub.{count.index}.id
+    subnet_id = aws_subnet.nomad-lab-pub["${var.aws_region}${var.az_map[count.index]}"].id
   
     #vpc_security_group_ids = [aws_security_group.nomad-sg.id]
     associate_public_ip_address = true
