f37c114224f1d7d51c025a37612a0dbd18c2b571
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 22:36:28 -0400

    update

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ resource "aws_instance" "nomad-node" {
     instance_type = var.nomad_node_instance_size
     key_name = var.aws_key_name
 
-    for_each = aws_subnet.nomad-lab-pub
-    subnet_id = each.value.id
+    #for_each = aws_subnet.nomad-lab-pub
+    #subnet_id = each.value.id
+    #subnet_id = aws_subnet.nomad-lab-pub.{count.index}.id
   
     vpc_security_group_ids = [ aws_security_group.nomad-sg.id ]
     associate_public_ip_address = true
