cb3407db0ee893555a8715c3c185608da9c4ad10
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 22:04:17 -0400

    updating vpc and network configuration

--- vars.tf
+++ vars.tf
@@ -10,10 +10,6 @@ variable "aws_region" {
 	description = "The region name to deploy into"
 }
 
-variable "aws_vpc_id" {
-	description = "VPC ID to launch into"
-}
-
 variable "aws_key_fingerprint" {
 	description = "Fingrprint of your SSH key"
 }
@@ -28,11 +24,6 @@ variable "nomad_node_instance_size" {
 
 variable "nomad_node_ami_id" {
 	description = "AMI ID to use for Nomad nodes"
-}
-
-variable "aws_subnet_id" {
-	description = "Default subnet to launch our nodes into"
-	type = list(string)
 }
 
 variable "nomad_node_count" {
