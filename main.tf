11f5ebccefc6279593f8f2ab0d12f90da55fd899
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 22:24:57 -0400

    fixing subnet

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "nomad-node" {
     instance_type = var.nomad_node_instance_size
     key_name = var.aws_key_name
     #subnet_id = aws_subnet_id[count.index]
-    subnet_id = aws_subnet.nomad-lab-pub.[count.index].id 
+    subnet_id = aws_subnet.nomad-lab-pub.${[count.index]}.id 
     vpc_security_group_ids = [ aws_security_group.nomad-sg.id ]
     associate_public_ip_address = true
     user_data = file("install-nomad.sh")
