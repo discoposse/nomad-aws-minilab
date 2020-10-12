73b187aede44d1c1de99ad4ef543a7614b0ae243
Eric Wright <eric.wright@turbonomic.com>
2020-10-12 08:22:49 -0400

    fixing subnets

--- network.tf
+++ network.tf
@@ -30,9 +30,10 @@ data "aws_subnet_ids" "nomad_subnets" {
         values = ["nomad-lab"]
     }
 }
- resource "aws_route_table_association" "private_rt_assoc_1a" {
-    count = "${length(data.aws_subnet_ids.nomad_subnets.ids)}"
-    subnet_id = tolist(data.aws_subnet_ids.nomad_subnets.ids, count.index)
+
+resource "aws_route_table_association" "private_rt_assoc_1a" {
+    count = length(data.aws_subnet_ids.nomad_subnets.ids)
+    subnet_id = element(data.aws_subnet_ids.nomad_subnets.ids, 0)
     route_table_id = aws_route_table.nomad-lab-public-crt.id
  }
 
