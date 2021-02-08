4d675eec657c661dc6b202fdc39375086a5433cd
Eric Wright <eric.wright@turbonomic.com>
2021-02-07 20:35:57 -0500

    acl work

--- outputs.tf
+++ outputs.tf
@@ -14,14 +14,10 @@ output "nomad_url" {
   	value = "http://${aws_instance.nomad-node[0].public_ip}:4646/ui"
 }
 
-output "consul_token" {
-	value = var.consul_token
-}
-
-output "nomad_token" {
-	value = var.nomad_token
-}
-
 output "ec2_nodes" {
 	value = aws_instance.nomad-node[*].public_dns
+}
+
+output "connection_info" {
+	value = "ssh -i aws_instance.nomad-node[*].key_name aws_instance.nomad-node[*].public_dns
 }
